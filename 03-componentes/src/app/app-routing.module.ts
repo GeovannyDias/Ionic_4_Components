d7fcfbcaf4dcfe9b3ea98f0e2c71c83a7d871e1c
GeovannyDias <geova_jd@hotmail.com>
2019-10-01 06:28:45 -0500

    popover

--- 03-componentes/src/app/app-routing.module.ts
+++ 03-componentes/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: 'alert', loadChildren: './pages/alert/alert.module#AlertPageModule' },
   { path: 'action-sheet', loadChildren: './pages/action-sheet/action-sheet.module#ActionSheetPageModule' },
   { path: 'modal', loadChildren: './pages/modal/modal.module#ModalPageModule' },
+  { path: 'popover', loadChildren: './pages/popover/popover.module#PopoverPageModule' },
   //{ path: 'modal-info', loadChildren: './pages/modal-info/modal-info.module#ModalInfoPageModule' },
 ];
 
